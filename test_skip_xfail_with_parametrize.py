2cc4bacef1b949b8a67819eac58b7e8539db5c41
wangmengmeng <wmm_0165@163.com>
2019-10-26 21:25:09 +0800

    skip and xfail

---
+++ test_skip_xfail_with_parametrize.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/10/26 12:12
+# @Author : wangmengmeng
+import pytest
+import sys
+
+
+@pytest.mark.parametrize(('n', 'expected'),
+                         [(1, 2),
+                          pytest.param(1, 0, marks=pytest.mark.xfail),
+                          pytest.param(1, 3, marks=pytest.mark.xfail(reason="some bug")),
+                          (2, 3),
+                          (3, 4),
+                          (4, 5),
+                          pytest.param(10, 11, marks=pytest.mark.skipif(sys.version_info >= (3, 0), reason="py2k"))])
+def test_increment(n, expected):
+    assert n + 1 == expected
